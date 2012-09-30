bd36766c7232038a48362c1ff46f08a3d16d2dbc
Konstantin Ilchenko <simpli4eg@gmail.com>
2012-10-01 00:43:01 +0300

    added user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,32 +1,39 @@
 require 'spec_helper'
 
-describe 'validators' do
+describe 'user model' do
   before(:each) do
     @user = User.create!(login: "sim", email: "sim@sim.sim", password: "123", two_step_auth: false)
   end
 
-  it "should create user with right attrs" do
-    User.create!(login: "simpl", email: "sim@simpl.sim", password: "123", two_step_auth: false).should
-    change(User, :count).by(2)
+  describe 'validators' do
+
+    it "should create user with right attrs" do
+      expect {
+        User.create(login: "simpl", email: "sim@simpl.sim", password: "123", two_step_auth: false)
+      }.to change(User, :count).by(1)
+    end
+
+    it "should not create user with same login" do
+      User.create(login: "sim", email: "sim@simpl.sim", password: "123", two_step_auth: false).should_not
+      change(User, :count)
+    end
+
+    it "should not create user with same email" do
+      User.create(login: "simpl", email: "sim@sim.sim", password: "123", two_step_auth: false).should
+      change(User, :count).by(1)
+    end
   end
 
-  it "should not create user with same login" do
-    User.create!(login: "sim", email: "sim@simpl.sim", password: "123", two_step_auth: false).should_not
-    change(User, :count)
+  describe 'authentication' do
+
+    it "should authenticate with correct password" do
+      User.authenticate("sim@sim.sim", "123").login.should == @user.login
+    end
+
+    it "should not authenticate with wrong password" do
+      User.authenticate("sim@sim.sim", "1234").should == false
+    end
+
   end
 
-  it "should not create user with same email" do
-    User.create(login: "simpl", email: "sim@sim.sim", password: "123", two_step_auth: false).should
-    change(User, :count).by(1)
-  end
-
-  it "should create user with right attrs" do
-    User.create(login: "simpl", email: "sim@simpl.sim", password: "123", two_step_auth: false).should
-    change(User, :count).by(1)
-  end
-
-  it "should create user with right attrs" do
-    User.create(login: "simpl", email: "sim@simpl.sim", password: "123", two_step_auth: false).should
-    change(User, :count).by(1)
-  end
 end
