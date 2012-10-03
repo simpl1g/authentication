6c7c1a062cd292a9151859f168226859af358d39
Konstantin Ilchenko <simpli4eg@gmail.com>
2012-10-03 11:44:19 +0300

    changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Authentication::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.enabled = true
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
@@ -43,7 +45,7 @@ Authentication::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
