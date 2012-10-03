b8e625dfab4fba54fa32a54f103a8917125a4bb8
Konstantin Ilchenko <simpli4eg@gmail.com>
2012-10-03 11:16:31 +0300

    changed initializers/asset_sync.rb

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,9 +1,9 @@
 AssetSync.configure do |config|
   config.fog_provider = 'AWS'
-  config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-  config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-  config.fog_directory = ENV['FOG_DIRECTORY']
-  
+  config.aws_access_key_id = AWS_ACCESS_KEY_ID
+  config.aws_secret_access_key = AWS_SECRET_ACCESS_KEY
+  config.fog_directory = FOG_DIRECTORY
+
   # Increase upload performance by configuring your region
   # config.fog_region = 'eu-west-1'
   #
@@ -13,7 +13,7 @@ AssetSync.configure do |config|
   # Automatically replace files with their equivalent gzip compressed version
   # config.gzip_compression = true
   #
-  # Use the Rails generated 'manifest.yml' file to produce the list of files to 
+  # Use the Rails generated 'manifest.yml' file to produce the list of files to
   # upload instead of searching the assets directory.
   # config.manifest = true
   #
