8ed0ce51747e65ad31b9cdf3bfe8550911f4d5da
Konstantin Ilchenko <simpli4eg@gmail.com>
2012-10-03 11:32:45 +0300

    assets_config

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,8 +1,8 @@
 AssetSync.configure do |config|
   config.fog_provider = 'AWS'
-  config.aws_access_key_id = 'AKIAI2Z2WLRZMPRW4C5Q'
-  config.aws_secret_access_key = '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'
-  config.fog_directory = 'ruby_gardens4'
+  config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
+  config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+  config.fog_directory = ENV['FOG_DIRECTORY']
 
   # Increase upload performance by configuring your region
   # config.fog_region = 'eu-west-1'
