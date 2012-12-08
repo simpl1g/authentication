70f6b4df0ca652def3430b7c7721a603b3897d91
simpl1g <simpli4eg@gmail.com>
2012-12-08 21:26:23 +0300

    Update app/controllers/oauth_controller.rb

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,25 +1,25 @@
 class OauthController < ApplicationController
+  before_filter :oauth
+  
   def start
-    oauth = get_oauth
-    redirect_to oauth.url_for_oauth_code(:permissions=>"email")
+    redirect_to @oauth.url_for_oauth_code(:permissions=>"email")
   end
 
   def callback
     if params[:error]
-      flash[:notice] = params[:error_description]
-      redirect_to login_path
+      redirect_to login_path, :notice => params[:error_description]
     else
-      oauth = get_oauth
-      graph = Koala::Facebook::API.new(oauth.get_access_token(params[:code]))
+      graph = Koala::Facebook::API.new(@oauth.get_access_token(params[:code]))
       email_and_name = graph.get_object("/me?fields=name,email")
-      user = User.find_from_facebook(email_and_name)
-      @user = sign_in_and_get_location user
+      @user = sign_in_and_get_location User.find_from_facebook(email_and_name)
       redirect_to @user
     end
   end
+  
+  private
 
-  def get_oauth
-    Koala::Facebook::OAuth.new(APP_ID, APP_SECRET,REDIRECT_URI)
+  def oauth
+    @oauth ||= Koala::Facebook::OAuth.new(APP_ID, APP_SECRET,REDIRECT_URI)
   end
 
 end
